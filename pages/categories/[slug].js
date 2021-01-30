1017fb256efa3d63970dfe86a8c564c1287079c5
loganwdev <53637763+loganwdev@users.noreply.github.com>
2021-01-30 07:05:33 -0600

    strapi plus snipcart223

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -5,9 +5,9 @@ import { getCategories, getCategory } from "../../utils/api";
 
 const CategoryPage = ({ category }) => {
   const router = useRouter();
-  // if (router.isFallback) {
-  //   return <div>Loading category...</div>;
-  // }
+  if (router.isFallback) {
+    return <div>Loading category...</div>;
+  }
 
   return (
     <div>
@@ -24,8 +24,8 @@ const CategoryPage = ({ category }) => {
 export default CategoryPage;
 
 export async function getStaticProps({ params }) {
-  // const category = await getCategory(params.slug);
-  // return { props: { category } };
+  const category = await getCategory(params.slug);
+  return { props: { category } };
 }
 
 export async function getStaticPaths() {

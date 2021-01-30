2d6e7da01a723926d04160ae02e7203315cb32e6
loganwdev <53637763+loganwdev@users.noreply.github.com>
2021-01-30 06:55:00 -0600

    strapi plus snipcart223

--- utils/api.js
+++ utils/api.js
@@ -12,10 +12,10 @@ export async function fetchAPI(path) {
   return data;
 }
 
-export async function getCategories() {
-  const categories = await fetchAPI("/categories");
-  return categories;
-}
+// export async function getCategories() {
+//   const categories = await fetchAPI("/categories");
+//   return categories;
+// }
 
 export async function getCategory(slug) {
   const categories = await fetchAPI(`/categories?slug=${slug}`); 

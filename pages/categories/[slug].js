879eccc492d5c9248508ae4aec9158c0f911d859
loganwdev <53637763+loganwdev@users.noreply.github.com>
2021-02-03 19:08:10 -0600

    strapi plus snipcart223456

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -29,7 +29,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const {categories} = await getCategories();
+  const categories = await getCategories();
   return {
     paths: categories.map((_category) => {
       return {
